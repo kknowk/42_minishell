2f36d971966de7f22efffaae308037c436bbfe50
minabe <minabe@student.42tokyo.jp>
2023-07-10 21:36:15 +0900

    fix: ヘッダーインクルード追加

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,19 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:24:12 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/10 20:52:00 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/10 21:08:09 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
