051a3a32883e400bc6508682d95c782987d8e71e
minabe <minabe@student.42tokyo.jp>
2023-07-10 21:57:16 +0900

    fix: 目指せlexer

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 20:26:51 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/10 21:34:49 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/10 21:51:45 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int	main(void)
 {
 	char	*line;
+	char	**lex;
 
 	line = readline("minishell > ");
 	if (line)
 		add_history(line);
+	lex = ft_split();
 	return (0);
 }
