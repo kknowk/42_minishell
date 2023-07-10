9ed9282e85a48a71e53345469f9668d91d72e28c
minabe <minabe@student.42tokyo.jp>
2023-07-10 20:52:35 +0900

    fix: minishell.h追加

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,11 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:24:12 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/10 18:24:13 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/10 20:52:00 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#endif
