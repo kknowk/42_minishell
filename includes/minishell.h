a5084ca2bc7f9b5d765724eb76b1ceee9c476223
minabe <minabe@student.42tokyo.jp>
2023-07-10 21:44:09 +0900

    feat: t_token追加

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:24:12 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/10 21:08:09 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/10 21:43:21 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@
 # include <sys/types.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_token	t_token;
+
+struct	s_token
+{
+	// t_token_type	type;
+	char			*data;
+	t_token			*prev;
+	t_token			*next;
+};
 
 #endif
