ee94e33b00190dcaa041ffdd72eedfa848ef54fd
minabe <minabe@student.42tokyo.jp>
2023-07-11 18:43:20 +0900

    #2 fix: 簡単なlexer

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,23 +6,26 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:17:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/11 16:39:47 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/11 18:08:40 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-int	lexer(char *line)
+t_token	lexer(char *str)
 {
 	size_t	i;
+	size_t	j;
 	t_token	*token;
 
 	i = 0;
 	while (line[i] != '\0')
 	{
-		/* pipeで分けてtokenごとにt_tokenへ保存する */
-		
-		i++;
+		j = 0;
+		while (str[i + j] != ' ' && str[i + j] != '\0')
+			j++;
+		tokenlistadd_back(ft_substr(str, i, j));
+		i += j;
 	}
-	return (0);
+	return (token);
 }
