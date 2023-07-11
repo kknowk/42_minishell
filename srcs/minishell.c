0fa7f2b8fdb80f3cce471d641625555e1f93188d
minabe <minabe@student.42tokyo.jp>
2023-07-11 16:46:15 +0900

    fix: minishellを無限ループ化

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:16:03 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/11 16:01:48 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/11 16:43:26 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,34 @@ void	minishell(char *envp[])
 	t_token	*token;
 
 	(void)envp;
-	line = readline("minishell > ");
-	if (line)
-		add_history(line);
-	/* "ctrl-C, -Z, -/" の処理追加する */
-	token = lexer(line);
+	rl_outstream = stderr; // defoultがstdoutのため
+	while (true)
+	{
+		line = readline("minishell$ ");
+		if (line == NULL)
+			break ;
+		else
+			add_history(line); // lineが'\0'のときは履歴に登録しない
+		token = lexer(line);
+		/* "ctrl-C, -Z, -/" の処理 */
+		// if (signal(SIGINT, ) == SIG_ERR)
+		// {
+		// 	/* 出力 */
+		// 	exit(EXIT_FAILURE);
+		// }
+		// if (signal(SIGQUIT, ) == SIG_ERR)
+		// {
+		// 	/* 出力 */
+		// 	exit(EXIT_FAILURE);
+		// }
 
-	// debug
-	for (int i = 0; token->next != NULL; i++)
-		printf("str: %s\n", token->data);
+		// debug
+		for (int i = 0; token->next != NULL; i++)
+			printf("str: %s\n", token->data);
 
-	tokenlist_clear(token);
+		/* ここにpercerやエクスパンション、コマンド実行を書く */
+
+		tokenlist_clear(token);
+		free(line);
+	}
 }
