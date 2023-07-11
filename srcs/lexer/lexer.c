bad4b290d51bb7d4c759a07c5c8b018dfde2dba7
Minato Abe <minato.cat.love@gmail.com>
2023-07-11 23:45:59 +0900

    #2 fix: tokenに変更

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,25 +6,25 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:17:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/11 18:08:40 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/11 21:49:44 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-t_token	lexer(char *str)
+t_token	*lexer(char *str)
 {
 	size_t	i;
 	size_t	j;
 	t_token	*token;
 
 	i = 0;
-	while (line[i] != '\0')
+	while (str[i] != '\0')
 	{
 		j = 0;
 		while (str[i + j] != ' ' && str[i + j] != '\0')
 			j++;
-		tokenlistadd_back(ft_substr(str, i, j));
+		tokenlistadd_back(token, ft_substr(str, i, j));
 		i += j;
 	}
 	return (token);
