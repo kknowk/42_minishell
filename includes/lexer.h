a5e27bf9d5eb905c2a2e7fb6b5751172face99ca
minabe <minabe@student.42tokyo.jp>
2023-07-11 17:01:34 +0900

    fix: t_lexerの内容追加

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:24:12 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/11 16:14:41 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/11 16:49:08 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 
 typedef struct	s_lexer
 {
-	bool	is_quoted;
+	size_t			str_len;
+	bool			is_quoted;
+	t_token_status	status;
 }	t_lexer;
 
 int	lexer(char *line);
