ef9641e5a1f3294683ff91f128c3d19d2af30353
minabe <minabe@student.42tokyo.jp>
2023-07-12 18:10:18 +0900

    fix: 微修正

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:17:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/12 18:02:14 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/12 18:10:01 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_lexer	*init_lexer(t_lexer *lex, char *str)
 t_token	*lexer(char *str)
 {
 	size_t	i;
-	size_t	j;
+	// size_t	j;
 	t_lexer lex;
 
 	init_lexer(&lex, str);
@@ -38,10 +38,11 @@ t_token	*lexer(char *str)
 	{
 		while (str[i] == ' ')
 			i++;
-		j = 0;
-		while (str[i + j + 1] != )
-		lex.token = tokenlistadd_back(lex.token, ft_substr(str, i, j));
-		i += j;
+		// j = 0;
+		// while (str[i + j + 1] != )
+		// lex.token = tokenlistadd_back(lex.token, ft_substr(str, i, j));
+		i++;
+		// i += j;
 	}
 	return (lex.list_head);
 }
