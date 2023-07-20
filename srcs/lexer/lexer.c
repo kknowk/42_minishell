141a4b321499ea9910cc04f51653007fce5789b5
minabe <minabe@student.42tokyo.jp>
2023-07-20 18:46:36 +0900

    #41 fix: おそらくcomplete

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:17:39 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/20 18:22:04 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/20 18:45:37 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static void	lexer_error(t_lexer *lex, char *str)
 
 	printf("%s\n", str);
 	token = lex->list_head;
-	while(token != NULL)
+	while (token != NULL)
 	{
 		tmp = token->next;
 		free(token);
@@ -64,10 +64,10 @@ static void	tokenize(t_lexer *lex, char *str)
 	char	c;
 
 	start = lex->word_start;
-	while(str[start + lex->word_len] != '\0')
+	while (str[start + lex->word_len] != '\0')
 	{
 		c = str[start + lex->word_len];
-		if (c == '|' || c == '<' || c == '>')
+		if (lex->word_len == 0 && (c == '|' || c == '<' || c == '>'))
 		{
 			if (is_redirect(c) && str[start + lex->word_len + 1] == c)
 				lex->word_len += D_REDIR_SIZE;
@@ -75,6 +75,8 @@ static void	tokenize(t_lexer *lex, char *str)
 				lex->word_len++;
 			break ;
 		}
+		else if (c == '|' || c == '<' || c == '>')
+			break ;
 		switch_quote_state(lex, c);
 		if (is_whitespace(c) && lex->is_quoted == false)
 			break ;
@@ -87,7 +89,7 @@ static void	tokenize(t_lexer *lex, char *str)
 
 t_token	*lexer(char *str)
 {
-	t_lexer lex;
+	t_lexer	lex;
 
 	init_lexer(&lex);
 	lex.word_start = 0;
@@ -102,7 +104,7 @@ t_token	*lexer(char *str)
 		tokenize(&lex, str);
 		lex.word_start += lex.word_len;
 	}
-	if(DEBUG)
+	if (DEBUG)
 		debug_lexer(&lex);
 	return (lex.list_head);
 }
