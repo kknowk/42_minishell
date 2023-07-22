dc8aa6ea01c8ae851a3b9ad0f25b06510111903c
minabe <minabe@student.42tokyo.jp>
2023-07-22 12:46:51 +0900

    fix: 微修正

--- includes/token.h
+++ includes/token.h
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:13:54 by minabe            #+#    #+#             */
-/*   Updated: 2023/07/22 12:36:52 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/22 12:46:14 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include "minishell.h"
 
-// \tや\0は無視??
 typedef enum e_tokentype
 {
 	CHAR_GENERAL = -1,
