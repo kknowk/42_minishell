dad20d31da0d751f888443e43d67e2a12334fcbf
khorike <horike.kaisyu@gmail.com>
2023-07-23 18:01:44 +0900

    fix:cd

--- srcs/builtins/ms_cd.c
+++ srcs/builtins/ms_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: khorike <khorike@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 14:08:10 by khorike           #+#    #+#             */
-/*   Updated: 2023/07/22 20:51:05 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/23 17:59:37 by khorike          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static int	check_permission(char *path)
 
 	if (stat(path, &statbuf) != 0)
 	{
-		write(STDERR_FILENO, "cd: no such file or directory", 29);
+		write(STDERR_FILENO, "cd: No such file or directory", 29);
 		write(STDERR_FILENO, ": ", 2);
 		write(STDERR_FILENO, path, ft_strlen(path));
 		write(STDERR_FILENO, "\n", 1);
@@ -42,7 +42,7 @@ static int	check_fd_o_dir(char *path)
 	fd = open(path, O_RDONLY);
 	if (fd > 0)
 	{
-		write(STDERR_FILENO, "cd: not a directory", 19);
+		write(STDERR_FILENO, "cd: Not a directory", 19);
 		write(STDERR_FILENO, ": ", 2);
 		write(STDERR_FILENO, path, ft_strlen(path));
 		write(STDERR_FILENO, "\n", 1);
@@ -71,7 +71,7 @@ static int	home(t_directory *dir, t_env_var **head)
 	home = search(head, "HOME");
 	if (!home)
 	{
-		perror("cd: HOME not set\n");
+		write(STDERR_FILENO, "cd: HOME not set\n", 18);
 		return (FAILURE);
 	}
 	chdir(home);
