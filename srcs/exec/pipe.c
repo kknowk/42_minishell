9511fa4e871e1abf533b51dc702235c080dc5c92
khorike <horike.kaisyu@gmail.com>
2023-07-30 11:35:18 +0900

    fix:norm

--- srcs/exec/pipe.c
+++ srcs/exec/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: khorike <khorike@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 17:58:34 by khorike           #+#    #+#             */
-/*   Updated: 2023/07/27 18:51:10 by minabe           ###   ########.fr       */
+/*   Updated: 2023/07/30 10:30:35 by khorike          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,28 @@ static void	close_wait(int pipefd[2], pid_t pid1, pid_t pid2)
 }
 
 static void	p1(t_node *node, t_directory *dir,
-		t_env_var **env_vars, int pipefd[2], int *error)
+		t_env_var **env_vars, int pipefd[2])
 {
 	close(pipefd[0]);
 	dup2(pipefd[1], STDOUT_FILENO);
 	close(pipefd[1]);
 	if (node->left != NULL)
-		execution(node->left, dir, env_vars, error);
+		execution(node->left, dir, env_vars);
 	exit(0);
 }
 
 static void	p2(t_node *node, t_directory *dir,
-		t_env_var **env_vars, int pipefd[2], int *error)
+		t_env_var **env_vars, int pipefd[2])
 {
 	close(pipefd[1]);
 	dup2(pipefd[0], STDIN_FILENO);
 	close(pipefd[0]);
 	if (node->right != NULL)
-		execution(node->right, dir, env_vars, error);
+		execution(node->right, dir, env_vars);
 	exit(0);
 }
 
-void	exec_pipe(t_node *node, t_directory *dir, t_env_var **env_vars, int *error)
+void	exec_pipe(t_node *node, t_directory *dir, t_env_var **env_vars)
 {
 	int		pipefd[2];
 	pid_t	pid1;
@@ -51,9 +51,9 @@ void	exec_pipe(t_node *node, t_directory *dir, t_env_var **env_vars, int *error)
 	pipe(pipefd);
 	pid1 = fork();
 	if (pid1 == 0)
-		p1(node, dir, env_vars, pipefd, error);
+		p1(node, dir, env_vars, pipefd);
 	pid2 = fork();
 	if (pid2 == 0)
-		p2(node, dir, env_vars, pipefd, error);
+		p2(node, dir, env_vars, pipefd);
 	close_wait(pipefd, pid1, pid2);
 }
